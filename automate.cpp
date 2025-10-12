68e22b5036da68a1303d708df76ae0d65a1f9ec7
Nasr-allah <65375745+sudo-LuSer@users.noreply.github.com>
2025-10-12 18:23:08 +0200

    Update automate.cpp
    plus lisible, maintenable et utilise des conventions de nommage standard pour les automates finis déterministes.

--- automate.cpp
+++ automate.cpp
@@ -1,46 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-class automata_TYPEDFA{
-	public: 
-		char q0; 
-		map<pair<char , char> , char> delta; 
-		char F; 
-		bool run(string word , char etat){
-			if(word=="" && F== etat){
-				return true; 
-			}
-			else if(word=="" && F != etat){
-				return false;
-			}
-			char w = (word[0]);
-			word = word.substr(1 , word.size());
-			return run(word , delta[make_pair(etat , w)]);
-		}
+
+class DFA {
+public:
+    char initial_state;
+    map<pair<char, char>, char> transitions;
+    char final_state;
+
+    bool process(const string& input, char current_state) {
+        if (input.empty()) {
+            return current_state == final_state;
+        }
+        
+        char symbol = input[0];
+        char next_state = transitions[{current_state, symbol}];
+        return process(input.substr(1), next_state);
+    }
 };
-int main(){
-	automata_TYPEDFA a; 
-	a.q0 = 'a';
-	a.F = 'b';
-	a.delta[make_pair('a' , '1')] = 'a';
-	a.delta[make_pair('a' , '3')] = 'a';
-	a.delta[make_pair('a' , '5')] = 'a';
-	a.delta[make_pair('a' , '7')] = 'a';
-	a.delta[make_pair('a' , '9')] = 'a';
-	a.delta[make_pair('a' , '0')] = 'b';
-	a.delta[make_pair('a' , '2')] = 'b';
-	a.delta[make_pair('a' , '4')] = 'b';
-	a.delta[make_pair('a' , '6')] = 'b';
-	a.delta[make_pair('a' , '8')] = 'b';
-	a.delta[make_pair('b' , '1' )] = 'a';
-	a.delta[make_pair('b' , '3' )] = 'a';
-	a.delta[make_pair('b' , '5' )] = 'a';
-	a.delta[make_pair('b' , '7' )] = 'a';
-	a.delta[make_pair('b' , '9' )] = 'a';
-	a.delta[make_pair('b' , '0')] = 'b';
-	a.delta[make_pair('b' , '2')] = 'b';
-	a.delta[make_pair('b' , '4')] = 'b';
-	a.delta[make_pair('b' , '6')] = 'b';
-	a.delta[make_pair('b' , '8')] = 'b';
-	string w = "10100001010101010000001000100100001010000101010001010101010100101018210731973918378917398137918378100001010100101010101001000111975";
-	cout<<a.run(w , a.q0)<<endl;
+
+int main() {
+    DFA automaton;
+    automaton.initial_state = 'a';
+    automaton.final_state = 'b';
+
+    for (char state : {'a', 'b'}) {
+        for (char odd : {'1', '3', '5', '7', '9'}) {
+            automaton.transitions[{state, odd}] = 'a';
+        }
+        for (char even : {'0', '2', '4', '6', '8'}) {
+            automaton.transitions[{state, even}] = 'b';
+        }
+    }
+
+    string test_input = "10100001010101010000001000100100001010000101010001010101010100101018210731973918378917398137918378100001010100101010101001000111975";
+    cout << automaton.process(test_input, automaton.initial_state) << endl;
+
+    return 0;
 }
